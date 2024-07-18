50c67b87e19d64f8a0ef8be25ebdcfd13e7bf540
19984646 <busetaktas45@gmail.com>
2024-07-18 04:23:07 +0330

    feat: routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,24 @@
-import { useEffect } from "react";
 import reactLogo from "./assets/react.svg";
-import viteLogo from "./assets/vite.svg";
+import viteLogo from "/vite.svg";
 import "./App.css";
 import { Link, Outlet } from "react-router-dom";
 import SimpleBottomNavigation from "./pages/Navigation";
+import { useEffect } from "react";
 import WebApp from "@twa-dev/sdk";
 
 function App() {
     useEffect(() => {
         // Telegram WebApp'i tam ekran moduna genişlet
         WebApp.expand();
-
-        // Telegram Mini App yüklendiğinde, alt kısımdaki öğeleri gizle
-        const hideTelegramBottom = () => {
-            const chatElement = document.querySelector('.tg_head_peer') as HTMLElement | null;
-            if (chatElement) {
-                chatElement.style.display = 'none';
-            }
-
-            const attachmentElement = document.querySelector('.composer_rich_textarea') as HTMLElement | null;
-            if (attachmentElement) {
-                attachmentElement.style.display = 'none';
-            }
-        };
-
-        hideTelegramBottom();
     }, []);
 
     return (
-        <div className="App">
+        <>
             <div>
-                <a href="https://vitejs.dev" target="_blank" rel="noopener noreferrer">
+                <a href="https://vitejs.dev" target="_blank">
                     <img src={viteLogo} className="logo" alt="Vite logo" />
                 </a>
-                <a href="https://react.dev" target="_blank" rel="noopener noreferrer">
+                <a href="https://react.dev" target="_blank">
                     <img src={reactLogo} className="logo react" alt="React logo" />
                 </a>
             </div>
@@ -51,7 +36,7 @@ function App() {
 
             <Outlet />
             <SimpleBottomNavigation />
-        </div>
+        </>
     );
 }
 
