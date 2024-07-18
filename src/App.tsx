bf1bef8bc2096dc3ae28d7678dde9241b35ecb6f
19984646 <busetaktas45@gmail.com>
2024-07-18 03:43:43 +0330

    feat: routing

--- src/App.tsx
+++ src/App.tsx
@@ -3,38 +3,41 @@ import viteLogo from "/vite.svg";
 import "./App.css";
 import { Link, Outlet } from "react-router-dom";
 import SimpleBottomNavigation from "./pages/Navigation";
+import { useEffect } from "react";
+import WebApp from "@twa-dev/sdk";
 
 function App() {
-  
+    useEffect(() => {
+        // Telegram WebApp'i tam ekran moduna genişlet
+        WebApp.expand();
+    }, []);
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
+    return (
+        <>
+            <div>
+                <a href="https://vitejs.dev" target="_blank">
+                    <img src={viteLogo} className="logo" alt="Vite logo" />
+                </a>
+                <a href="https://react.dev" target="_blank">
+                    <img src={reactLogo} className="logo react" alt="React logo" />
+                </a>
+            </div>
+            <h1>Vite + React</h1>
 
-      <nav>
-        <Link to="/vite-react-router/">Home</Link>
-        {" | "}
-        <Link to="/vite-react-router/farm">Farm</Link>
-        {" | "}
-        <Link to="/vite-react-router/user-details">User Profile</Link>
-        {" | "}
-        <Link to="/vite-react-router/user-profile-page">User Profile Page</Link>
-      </nav>
+            <nav>
+                <Link to="/vite-react-router/">Home</Link>
+                {" | "}
+                <Link to="/vite-react-router/farm">Farm</Link>
+                {" | "}
+                <Link to="/vite-react-router/user-details">User Profile</Link>
+                {" | "}
+                <Link to="/vite-react-router/user-profile-page">User Profile Page</Link>
+            </nav>
 
-      <Outlet />
-      <SimpleBottomNavigation />
-
-
-    </>
-  );
+            <Outlet />
+            <SimpleBottomNavigation />
+        </>
+    );
 }
 
 export default App;
