d5252901e6441c7fd9709c5ed3c61abf59c23f63
19984646 <busetaktas45@gmail.com>
2024-07-19 01:20:16 +0330

    feat: routing

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,3 @@
 export function Home() {
-  return <h2>Home</h2>;
+  return <h2>Home Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis quos consequuntur quae placeat reiciendis rerum quas dignissimos maxime eligendi laudantium. Consequuntur, odit ullam repellendus distinctio inventore quia voluptas ipsa itaque.</h2>;
 }
