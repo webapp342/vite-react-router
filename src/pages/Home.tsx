c3be7a7fbe4aa91656aefc3faad0b7dd56bcedaf
19984646 <busetaktas45@gmail.com>
2024-07-19 02:07:21 +0330

    feat: routing

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,13 @@
 export function Home() {
-  return <h2>Home Lorem ipsum dolor, sit amet consectetur adipisicing elit. Neque illum, maiores laboriosam aperiam ullam animi Lorem, ipsum dolor sit amet consectetur adipisicing elit. Deserunt, cum velit. Sint neque quaerat nesciunt expedita consectetur quod odio, id molestias architecto, aperiam autem deleniti iusto earum soluta rem est! quidem enim sunt explicabo aliquam corrupti nam culpa porro id iure perferendis dolores recusandae expedita? Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis quos consequuntur quae placeat reiciendis rerum quas dignissimos maxime eligendi laudantium. Consequuntur, odit ullam repellendus distinctio inventore quia voluptas ipsa itaque.</h2>;
+  return <div>
+    <h2>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusantium, repudiandae minus. Ut, totam iure? Veniam, sapiente! Accusantium fuga officiis, excepturi dolore neque nulla sapiente porro, officia velit quod ratione impedit.</h2>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi laudantium minus fuga iusto numquam ratione aut sapiente minima, voluptate quis eius. Dolore aut atque minima doloremque non suscipit accusamus rerum.
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi alias velit ut, architecto voluptates dolore esse facere animi consectetur quam odit quia adipisci dignissimos qui placeat dolorem error distinctio pariatur.
+    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Doloribus illo, ullam, voluptates praesentium architecto culpa nisi voluptatem repellendus odit doloremque blanditiis autem enim quae laboriosam aspernatur! Recusandae exercitationem rerum tenetur.
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos repellendus distinctio in deserunt accusamus similique quibusdam odit quis omnis! Delectus suscipit maiores error. At similique inventore excepturi soluta vel temporibus!
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio, neque! Enim, quas minus maxime eligendi, earum amet consequuntur vitae, dolores soluta voluptate accusantium voluptas officia! Aliquam at error vero quae?</p>
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis, odio! Architecto quo repudiandae quis, a at doloremque quibusdam nihil velit voluptas obcaecati perferendis repellendus. Minima repellat soluta beatae voluptate possimus?</p>
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illum porro at dolores rerum amet eum quae. Animi enim soluta ex! Enim vero soluta tempore, ex aliquid aperiam ipsum. Qui, harum.</p>
+  </div>
+
 }
