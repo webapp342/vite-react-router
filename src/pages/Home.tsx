3dd3fd1f400480fe5b7ada9b00ea7f1335e7e5b9
19984646 <busetaktas45@gmail.com>
2024-07-19 02:37:25 +0330

    feat: routing

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,13 +1,7 @@
 export function Home() {
   return             <div className="main-content">
+    <h1>ddsdsdsd</h1>
 
-    <h2>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusantium, repudiandae minus. Ut, totam iure? Veniam, sapiente! Accusantium fuga officiis, excepturi dolore neque nulla sapiente porro, officia velit quod ratione impedit.</h2>
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi laudantium minus fuga iusto numquam ratione aut sapiente minima, voluptate quis eius. Dolore aut atque minima doloremque non suscipit accusamus rerum.
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi alias velit ut, architecto voluptates dolore esse facere animi consectetur quam odit quia adipisci dignissimos qui placeat dolorem error distinctio pariatur.
-    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Doloribus illo, ullam, voluptates praesentium architecto culpa nisi voluptatem repellendus odit doloremque blanditiis autem enim quae laboriosam aspernatur! Recusandae exercitationem rerum tenetur.
-    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos repellendus distinctio in deserunt accusamus similique quibusdam odit quis omnis! Delectus suscipit maiores error. At similique inventore excepturi soluta vel temporibus!
-<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio, neque! Enim, quas minus maxime eligendi, earum amet consequuntur vitae, dolores soluta voluptate accusantium voluptas officia! Aliquam at error vero quae?</p>
-<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis, odio! Architecto quo repudiandae quis, a at doloremque quibusdam nihil velit voluptas obcaecati perferendis repellendus. Minima repellat soluta beatae voluptate possimus?</p>
   </div>
 
 }
