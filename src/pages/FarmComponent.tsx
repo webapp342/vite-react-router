bccde23621a77ed1dce10d691a037d85e103854a
19984646 <busetaktas45@gmail.com>
2024-07-23 02:18:32 +0330

    Yerel değişikliklerimi kaydettim

--- src/pages/FarmComponent.tsx
+++ src/pages/FarmComponent.tsx
@@ -3,8 +3,6 @@ import { Box } from '@mui/material';
 import { styled } from '@mui/material/styles';
 import backgroundJpg from './background.jpg';
 import backgroundGif from './background.gif';
-import { doc, getDoc } from 'firebase/firestore';
-import { db } from './firebaseConfig';
 
 // Styled component for background
 const BackgroundBox = styled(Box)<{ isFarming: boolean }>(({ isFarming }) => ({
@@ -28,27 +26,8 @@ const IntegratedComponent: React.FC = () => {
   const [isFarming, setIsFarming] = useState<boolean>(false);
 
   useEffect(() => {
-    const fetchUserData = async () => {
-      const telegramUserId = localStorage.getItem('telegramUserId');
-      if (telegramUserId) {
-        try {
-          const docRef = doc(db, 'countdowns', telegramUserId);
-          const docSnap = await getDoc(docRef);
-          if (docSnap.exists()) {
-            const data = docSnap.data();
-            setIsFarming(data.isRunning);
-          } else {
-            console.error('No such document in Firestore!');
-          }
-        } catch (error) {
-          console.error('Error fetching user data: ', error);
-        }
-      } else {
-        console.error('Telegram user ID not found in localStorage');
-      }
-    };
-
-    fetchUserData();
+    const isRunning = localStorage.getItem('isRunning');
+    setIsFarming(isRunning === 'true');
   }, []);
 
   return (
