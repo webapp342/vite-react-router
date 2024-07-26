e0ed9341a35503bc9c6b5f3091ff536da8d77677
19984646 <busetaktas45@gmail.com>
2024-07-26 03:38:00 +0330

    Yerel değişikliklerimi kaydettim

--- src/pages/FarmComponent.tsx
+++ src/pages/FarmComponent.tsx
@@ -1,9 +1,10 @@
 import React, { useState, useEffect } from 'react';
-import { Box, Button } from '@mui/material';
+import { Box } from '@mui/material';
 import { styled } from '@mui/material/styles';
 import backgroundJpg from './background.jpg';
 import backgroundGif from './background.gif';
 import UserDetails from "./UserDetails.tsx";
+
 
 // Styled component for background
 const BackgroundBox = styled(Box)<{ isRunning: boolean }>(({ isRunning }) => ({
@@ -15,7 +16,9 @@ const BackgroundBox = styled(Box)<{ isRunning: boolean }>(({ isRunning }) => ({
   display: 'flex',
   justifyContent: 'center',
   alignItems: 'center',
-  backgroundImage: isRunning ? `url(${backgroundGif})` : `url(${backgroundJpg})`,
+  backgroundImage: isRunning
+    ? `url(${backgroundGif})`
+    : `url(${backgroundJpg})`,
   backgroundSize: 'cover',
   backgroundPosition: 'center',
   transition: 'background-image 1s ease',
@@ -24,48 +27,21 @@ const BackgroundBox = styled(Box)<{ isRunning: boolean }>(({ isRunning }) => ({
 const IntegratedComponent: React.FC = () => {
   const [isRunning, setIsRunning] = useState<boolean>(false);
 
-  // Function to handle localStorage updates
-  const updateIsRunning = () => {
+  useEffect(() => {
     const storedIsRunning = localStorage.getItem('isRunning');
-    if (storedIsRunning !== null) {
+    if (storedIsRunning === null) {
+      console.warn('No "isRunning" value found in local storage');
+    } else {
+      console.log(`Fetched "isRunning" value: ${storedIsRunning}`);
       setIsRunning(storedIsRunning === 'true');
     }
-  };
-
-  useEffect(() => {
-    // Initial fetch from localStorage
-    updateIsRunning();
-
-    // Event listener for storage changes
-    const handleStorageChange = (event: StorageEvent) => {
-      if (event.key === 'isRunning') {
-        updateIsRunning();
-      }
-    };
-
-    window.addEventListener('storage', handleStorageChange);
-
-    // Cleanup the event listener on component unmount
-    return () => {
-      window.removeEventListener('storage', handleStorageChange);
-    };
   }, []);
-
-  // Function to start the counter
-  const startCounter = () => {
-    localStorage.setItem('isRunning', 'true');
-    setIsRunning(true);
-    // Here you can also add your counter logic
-  };
 
   return (
     <BackgroundBox isRunning={isRunning}>
       <div className="main-content">
-        <UserDetails />
+      <UserDetails />
         <p><strong>Is Running:</strong> {isRunning ? 'Yes' : 'No'}</p>
-        <Button variant="contained" color="primary" onClick={startCounter} disabled={isRunning}>
-          Start Counter
-        </Button>
       </div>
     </BackgroundBox>
   );
