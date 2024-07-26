7b50a64aaef9c60d4c778cff6ac48423ec7f0b4a
19984646 <busetaktas45@gmail.com>
2024-07-27 02:45:58 +0330

    Yerel değişikliklerimi kaydettim

--- src/pages/firestoreService.ts
+++ src/pages/firestoreService.ts
@@ -1,14 +1,21 @@
 import { db } from './firebaseConfig';
-import { doc, updateDoc } from 'firebase/firestore';
+import { doc, getDoc, updateDoc } from 'firebase/firestore';
 
-export const updateSpinPoints = async (telegramUserId: string, spinPoints: number) => {
+export const updateSpinPoints = async (telegramUserId: string, newSpinPoints: number) => {
   const userRef = doc(db, 'users', telegramUserId);
 
   try {
-    await updateDoc(userRef, {
-      spinPoints: spinPoints
-    });
-    console.log("Spin points updated successfully for user:", telegramUserId);
+    const userDoc = await getDoc(userRef);
+    if (userDoc.exists()) {
+      const currentSpinPoints = userDoc.data().spinPoints || 0;
+      const updatedSpinPoints = currentSpinPoints + newSpinPoints;
+      await updateDoc(userRef, {
+        spinPoints: updatedSpinPoints
+      });
+      console.log("Spin points updated successfully for user:", telegramUserId);
+    } else {
+      console.error("User document does not exist");
+    }
   } catch (error) {
     console.error("Error updating spin points: ", error);
   }
