803adc3f72413f94ecc746bf2df167584e22662c
19984646 <busetaktas45@gmail.com>
2024-07-28 03:40:35 +0330

    Yerel değişikliklerimi kaydettim

--- src/pages/pointsManager.tsx
+++ src/pages/pointsManager.tsx
@@ -1,33 +1,48 @@
 import React, { useState, useEffect } from 'react';
 import { onSnapshot, doc } from 'firebase/firestore';
-import { db } from './firebaseConfig'; // Firebase konfigürasyonunuzu import edin
-import { Box, Typography, Paper } from '@mui/material'; // MUI bileşenleri
+import { db } from './firebaseConfig';
+import { Box, Typography, Paper } from '@mui/material';
 import SportsEsportsIcon from '@mui/icons-material/SportsEsports';
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
-import logo from '../assets/ton_logo_dark_background.svg'; // SVG dosyasını img olarak import edin
+import logo from '../assets/ton_logo_dark_background.svg';
+import { useSpring, animated } from '@react-spring/web';
 
 const PointsManager: React.FC = () => {
   const [spinPoints, setSpinPoints] = useState<number>(0);
   const [points, setPoints] = useState<number>(0);
+  const [prevSpinPoints, setPrevSpinPoints] = useState<number>(0);
+  const [prevPoints, setPrevPoints] = useState<number>(0);
   const userId = localStorage.getItem('telegramUserId') || '';
 
   useEffect(() => {
     if (userId) {
       const userRef = doc(db, 'users', userId);
 
-      // Veritabanı değişikliklerini dinle
       const unsubscribe = onSnapshot(userRef, (docSnapshot) => {
         if (docSnapshot.exists()) {
           const data = docSnapshot.data();
+          setPrevSpinPoints(spinPoints);
+          setPrevPoints(points);
           setSpinPoints(data?.spinPoints || 0);
           setPoints(data?.points || 0);
         }
       });
 
-      // Unsubscribe on component unmount
       return () => unsubscribe();
     }
-  }, [userId]);
+  }, [userId, spinPoints, points]);
+
+  const spinPointsAnimation = useSpring({
+    from: { number: prevSpinPoints },
+    to: { number: spinPoints },
+    config: { duration: 500 },
+  });
+
+  const pointsAnimation = useSpring({
+    from: { number: prevPoints },
+    to: { number: points },
+    config: { duration: 500 },
+  });
 
   const boxStyles = {
     padding: 1,
@@ -36,9 +51,9 @@ const PointsManager: React.FC = () => {
     flexDirection: 'row',
     backgroundColor: 'black',
     width: '27%',
-    height: '50px', // Sabit yükseklik
+    height: '50px',
     boxSizing: 'border-box',
-    justifyContent: 'center'
+    justifyContent: 'center',
   };
 
   return (
@@ -47,24 +62,28 @@ const PointsManager: React.FC = () => {
       top={0}
       left={0}
       width="100%"
-      p={1} // Daha küçük padding
-      zIndex={1000} // Diğer içeriklerin üstünde görünmesini sağlar
+      p={1}
+      zIndex={1000}
       display="flex"
-      justifyContent="space-between" // Kutuları eşit boşlukla yayar
+      justifyContent="space-between"
       alignItems="center"
-      sx={{ boxSizing: 'border-box' }} // Box'ın içerik boyutlarına göre ayarlanmasını sağlar
+      sx={{ boxSizing: 'border-box' }}
     >
       <Paper elevation={3} sx={{ ...boxStyles }}>
-        <SportsEsportsIcon sx={{ marginRight: 1, fontSize: '1rem', color: 'white' }} />
-        <Typography variant="body2" color="white">{spinPoints}</Typography>
+        <SportsEsportsIcon sx={{ marginRight: 1, fontSize: '1.25rem', color: 'white' }} />
+        <Typography variant="body2" color="white">
+          <animated.span>{spinPointsAnimation.number.to(n => n.toFixed(0))}</animated.span>
+        </Typography>
       </Paper>
       <Paper elevation={3} sx={{ ...boxStyles, width: '40%' }}>
         <AccountCircleIcon sx={{ marginRight: 1, fontSize: '1.25rem', color: 'white' }} />
         <Typography variant="body2" color="white">Profil</Typography>
       </Paper>
       <Paper elevation={3} sx={{ ...boxStyles }}>
-        <Typography variant="body2" color="white" sx={{ marginRight: 1 }}>{points}</Typography>
-        <img src={logo} alt="Ton Logo" style={{ width: 25, height: 25 }} /> {/* img etiketi ile SVG kullanımı */}
+        <Typography variant="body2" color="white" sx={{ marginRight: 1 }}>
+          <animated.span>{pointsAnimation.number.to(n => n.toFixed(0))}</animated.span>
+        </Typography>
+        <img src={logo} alt="Ton Logo" style={{ width: 28, height: 28 }} />
       </Paper>
     </Box>
   );
