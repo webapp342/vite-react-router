53b111e128100d3d97cead320382c9020ff75e1b
19984646 <busetaktas45@gmail.com>
2024-07-28 03:31:40 +0330

    Yerel değişikliklerimi kaydettim

--- src/pages/pointsManager.tsx
+++ src/pages/pointsManager.tsx
@@ -3,7 +3,8 @@ import { onSnapshot, doc } from 'firebase/firestore';
 import { db } from './firebaseConfig'; // Firebase konfigürasyonunuzu import edin
 import { Box, Typography, Paper } from '@mui/material'; // MUI bileşenleri
 import SportsEsportsIcon from '@mui/icons-material/SportsEsports';
-import StarIcon from '@mui/icons-material/Star';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import logo from '../assets/ton_logo_dark_background.svg'; // SVG dosyasını img olarak import edin
 
 const PointsManager: React.FC = () => {
   const [spinPoints, setSpinPoints] = useState<number>(0);
@@ -28,6 +29,18 @@ const PointsManager: React.FC = () => {
     }
   }, [userId]);
 
+  const boxStyles = {
+    padding: 1,
+    display: 'flex',
+    alignItems: 'center',
+    flexDirection: 'row',
+    backgroundColor: 'black',
+    width: '27%',
+    height: '50px', // Sabit yükseklik
+    boxSizing: 'border-box',
+    justifyContent: 'center'
+  };
+
   return (
     <Box
       position="fixed"
@@ -37,17 +50,21 @@ const PointsManager: React.FC = () => {
       p={1} // Daha küçük padding
       zIndex={1000} // Diğer içeriklerin üstünde görünmesini sağlar
       display="flex"
-      justifyContent="space-between" // Eşit şekilde yayılır ve uçlara yapışır
+      justifyContent="space-between" // Kutuları eşit boşlukla yayar
       alignItems="center"
       sx={{ boxSizing: 'border-box' }} // Box'ın içerik boyutlarına göre ayarlanmasını sağlar
     >
-      <Paper elevation={3} sx={{ padding: 1, display: 'flex', alignItems: 'center', flexDirection: 'row', backgroundColor: 'rgba(255, 255, 255, 0.7)' }}>
-        <SportsEsportsIcon sx={{ marginRight: 1 }} />
-        <Typography variant="h6" color="textPrimary">{spinPoints}</Typography>
+      <Paper elevation={3} sx={{ ...boxStyles }}>
+        <SportsEsportsIcon sx={{ marginRight: 1, fontSize: '1rem', color: 'white' }} />
+        <Typography variant="body2" color="white">{spinPoints}</Typography>
       </Paper>
-      <Paper elevation={3} sx={{ padding: 1, display: 'flex', alignItems: 'center', flexDirection: 'row', backgroundColor: 'rgba(255, 255, 255, 0.7)' }}>
-        <StarIcon sx={{ marginRight: 1 }} />
-        <Typography variant="h6" color="textPrimary">{points}</Typography>
+      <Paper elevation={3} sx={{ ...boxStyles, width: '40%' }}>
+        <AccountCircleIcon sx={{ marginRight: 1, fontSize: '1.25rem', color: 'white' }} />
+        <Typography variant="body2" color="white">Profil</Typography>
+      </Paper>
+      <Paper elevation={3} sx={{ ...boxStyles }}>
+        <Typography variant="body2" color="white" sx={{ marginRight: 1 }}>{points}</Typography>
+        <img src={logo} alt="Ton Logo" style={{ width: 25, height: 25 }} /> {/* img etiketi ile SVG kullanımı */}
       </Paper>
     </Box>
   );
