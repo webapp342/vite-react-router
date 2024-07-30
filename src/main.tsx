5183890c4d5832a83a86104b45cf924f41a6a47f
webapp342 <alirezadehqan356@gmail.com>
2024-07-30 16:24:02 +0330

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -31,7 +31,7 @@ const router = createBrowserRouter([
       },
       {
         path: "/vite-react-router/user-profile-page",
-        element: <UserProfilePage />,
+        element: <TumbleSlotGame />,
       },
     ],
   },
